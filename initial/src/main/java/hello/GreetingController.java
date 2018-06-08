782fa80548b12eb65c328a50677e59051b3c8876
akrasilnikov <meingipfel@mail.ru>
2018-06-08 10:59:32 +0300

    тесты

--- initial/src/main/java/hello/GreetingController.java
+++ initial/src/main/java/hello/GreetingController.java
@@ -1,19 +1,126 @@
 package hello;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hi my dear  %s!";
     private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    private static String UPLOADED_FOLDER = "F://temp//";
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    @RequestMapping(value = "/greeting2", method = RequestMethod.POST)
+    public Greeting greeting2(@RequestParam(value = "name", required = false, defaultValue = "    bitch!") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @PostMapping("/api/upload/multi")
+    public ResponseEntity<?> uploadFileMulti(
+            @RequestParam("extraField") String extraField,
+            @RequestParam("files") MultipartFile[] uploadfiles) {
+
+        // Get file name
+        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
+                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+
+        if (StringUtils.isEmpty(uploadedFileName)) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(uploadfiles));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - "
+                + uploadedFileName, HttpStatus.OK);
+
+    }
+    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+
+        for (MultipartFile file : files) {
+
+            if (file.isEmpty()) {
+                continue; //next pls
+            }
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+        }
+
+    }
+    /*@RequestMapping(value = "/uploadMyFile", method = RequestMethod.POST)
+    @ResponseBody
+    public String handleFileUpload(MultipartHttpServletRequest request)
+            throws Exception {
+        Iterator<String> itrator = request.getFileNames();
+        MultipartFile multiFile = request.getFile(itrator.next());
+        try {
+            // just to show that we have actually received the file
+            System.out.println("File Length:" + multiFile.getBytes().length);
+            System.out.println("File Type:" + multiFile.getContentType());
+            String fileName=multiFile.getOriginalFilename();
+            System.out.println("File Name:" +fileName);
+            String path=request.getServletContext().getRealPath("/");
+
+            //making directories for our required path.
+            byte[] bytes = multiFile.getBytes();
+            File directory=    new File(path+ "/uploads");
+            directory.mkdirs();
+            // saving the file
+            File file=new File(directory.getAbsolutePath()+System.getProperty("file.separator"));
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(file));
+            stream.write(bytes);
+            stream.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new Exception("Error while loading the file");
+        }
+        return toJson("File Uploaded successfully.");
+    }
+
+    public String toJson(Object data)
+    {
+        ObjectMapper mapper=new ObjectMapper();
+        StringBuilder builder=new StringBuilder();
+        try {
+            builder.append(mapper.writeValueAsString(data));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }*/
 }
